18004ba47cc60a511eb93ea37dce9cac04348223
Abel Elias <abel.elias100@gmail.com>
2023-07-10 21:03:02 -0300

    Entite_Product

--- course/src/main/java/com/educandoweb/course/repositories/CategoryRepository.java
+++ course/src/main/java/com/educandoweb/course/repositories/CategoryRepository.java
@@ -3,7 +3,6 @@ package com.educandoweb.course.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.educandoweb.course.entites.Category;
-import com.educandoweb.course.entites.User;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
 
